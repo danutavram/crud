840c4b91bdec59be29b7d39c2915833d308e84f1
danutavram <avramnicolaedan@gmail.com>
2024-03-17 15:05:53 +0200

    ...Read, Update & Delete

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -2,21 +2,60 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ToDoListController extends AbstractController
 {
-    #[Route('/', name: 'to_do_list')]
-    public function index()
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
     {
-        return $this->render('index.html.twig');
+        $this->em = $em;
     }
 
-    #[Route('/create', name: 'create_task', methods:'POST')]
+    #[Route('/', name: 'to_do_list')]
+    public function index(EntityManagerInterface $em)
+    {
+        $tasks = $em->getRepository(Task::class)->findBy([], ['id' => 'DESC']);
+        return $this->render('index.html.twig', ['tasks' => $tasks]);
+    }
+
+    #[Route('/create', name: 'create_task', methods: 'POST')]
     public function create(Request $request)
     {
-        exit('to do: create!');    
+        $title = trim($request->request->get('title'));
+        if (empty($title))
+            return $this->redirectToRoute('to_do_list');
+
+        $task = new Task();
+        $task->setTitle($title);
+
+        $this->em->persist($task);
+        $this->em->flush();
+
+        return $this->redirectToRoute('to_do_list');
+    }
+
+    #[Route('/switch-status/{id}', name: 'switch_status')]
+    public function switchStatus(Task $task)
+    {
+        $task->setStatus(!$task->isStatus());
+        $this->em->flush();
+
+        return $this->redirectToRoute('to_do_list');
+    }
+
+    #[Route('/delete/{id}', name: 'task_delete')]
+    public function delete(Task $task): RedirectResponse
+    {
+        $this->em->remove($task);
+        $this->em->flush();
+
+        return $this->redirectToRoute('to_do_list');
     }
 }
